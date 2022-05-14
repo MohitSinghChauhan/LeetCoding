5d71a4bd5ffccff8f407105faa28ab6bd92cc5b9
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2022-05-14 15:54:13 +0530

    Time: 3 ms (59.47%), Space: 6.3 MB (73.64%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -6,17 +6,17 @@ public:
                     
         if(n1==n2 and n1==1) return 0;
         
-        for(int i=0;i<n1;i++){
+        for(int i=0;i<=n1-n2;i++){
             if(n2>(n1-i)) return -1;
 
-        if(haystack[i]==needle[0] and n2<=(n1-i)){     
+        if(haystack[i]==needle[0]){     
             if(n2==1) return i;
             
-            for(int j=i,k=0;j<=n2,k<n2; j++,k++){
-                if(haystack[j]!=needle[k]){
+            for(int j=0;j<n2; j++){
+                if(haystack[i+j]!=needle[j]){
                     break;
                 }    
-                if(k==n2-1){
+                if(j==n2-1){
                     return i;
                 }
             }
