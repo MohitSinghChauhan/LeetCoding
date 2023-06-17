647d2dc685f9fd178d3685646a862cb98772c37b
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-06-18 00:09:14 +0530

    Time: 5 ms (83.77%), Space: 6.4 MB (92.28%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        string s = countAndSay(n-1);
+        string result = "";
+        int cnt=1;
+        int size = s.size();
+        for(int i= 0; i < size-1; i++){
+            if(s[i]==s[i+1]) cnt++;
+            else{
+                result.push_back('0'+cnt);
+                result.push_back(s[i]);
+                cnt=1;
+            }
+        }
+
+        result.push_back('0'+cnt);
+        result.push_back(s[size-1]);
+
+        return result;
+
+    }
+};
