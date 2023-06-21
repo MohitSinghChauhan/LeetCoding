5844e3bf06ebbce114543faf06163a3d88bd0ca6
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-06-21 05:58:10 +0530

    Time: 36 ms (52.14%), Space: 25.7 MB (17.26%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(k==nums.size()) return;
-        if(nums.size()<k){
-            k = k % nums.size();  
+        int n = nums.size();
+        if(k==n) return;
+        if(n<k){
+            k = k % n;  
+        }
+        vector<int> ans;
+        for(int i = n-k ; i<n; i++){
+            ans.push_back(nums[i]);
+        }
+        //Old code -- big blunder
+        //code you provided has a bug in the second for loop where you are copying elements from nums[i] to nums[i+k]. The problem is that you are overwriting the elements in nums before you have finished copying all the required elements.
+        // for(int i = 0; i<n-k; i++){
+        //     nums[i+k]=nums[i];
+        // }
+
+        // correct code
+        for(int i=n-k-1; i>=0; i-- ){
+            nums[i+k]=nums[i];
         }
 
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+(k));
-        reverse(nums.begin()+k,nums.end());
 
+        for(int i=0; i<k; i++){
+            nums[i]=ans[i];
+        }
     }
 };
