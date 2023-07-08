6b28df96d9110b2809575e23420545613c0b33da
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-07-09 03:34:05 +0530

    Time: 28 ms (92.82%), Space: 26.2 MB (6.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        if(k==n) return;
-        if(n<k){
-            k = k % n;  
-        }
         vector<int> ans;
-        for(int i = n-k ; i<n; i++){
+        if(k==nums.size()) return;
+        // int temp;
+        if(nums.size()<k){
+            k = k % nums.size();  
+        }
+        for(int i = nums.size()-k; i<nums.size(); i++){
             ans.push_back(nums[i]);
         }
-        //Old code -- big blunder
-        //code you provided has a bug in the second for loop where you are copying elements from nums[i] to nums[i+k]. The problem is that you are overwriting the elements in nums before you have finished copying all the required elements.
-        // for(int i = 0; i<n-k; i++){
-        //     nums[i+k]=nums[i];
-        // }
-
-        // correct code
-        for(int i=n-k-1; i>=0; i-- ){
-            nums[i+k]=nums[i];
+        for(int i = 0; i<nums.size()-k; i++){
+            ans.push_back(nums[i]);
         }
 
-
-        for(int i=0; i<k; i++){
+        for(int i=0; i<nums.size(); i++){
             nums[i]=ans[i];
         }
     }
