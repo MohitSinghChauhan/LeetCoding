ff587f8ef7874ef35b840188e2785e1865e90ee7
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-09-12 03:29:40 +0530

    Time: 2 ms (75.22%), Space: 6.5 MB (9.23%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
+        unordered_map<int,int> memo;
+        return utilFib(n,memo);
+    }
+
+private:
+    int utilFib(int n, unordered_map<int,int>& memo){
         if(n==0 || n==1)
         return n;
 
-        return fib(n-1)+fib(n-2);
+        if(memo.find(n)!=memo.end()){
+            return memo[n];
+        }
+        int output = utilFib(n-1, memo)+utilFib(n-2,memo);
+        memo[n]=output;
+        return output;
     }
 };
