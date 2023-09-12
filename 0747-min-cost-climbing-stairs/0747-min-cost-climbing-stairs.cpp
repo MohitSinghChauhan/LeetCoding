e72bf56ceccc1fb304129e71bff770ff45c8dcb8
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-09-12 07:28:07 +0530

    Time: 12 ms (7.67%), Space: 15.6 MB (5.02%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        unordered_map<int,int> memo;
+        int currentStair = 0;
+        return min(calMinCost(currentStair,cost,memo),calMinCost(currentStair+1,cost,memo));
+    }
+private:
+    int calMinCost(int currentStair, vector<int>& cost, unordered_map<int, int>& memo){
+        if(currentStair>=cost.size())
+            return 0;
+
+        if(memo.find(currentStair)!=memo.end()){
+            return memo[currentStair];
+        }
+
+        int minCostOneStep = cost[currentStair] + calMinCost(currentStair+1, cost, memo);
+        int minCostTwoStep = cost[currentStair] + calMinCost(currentStair+2, cost, memo);
+
+        int minCost = min(minCostOneStep, minCostTwoStep);
+        memo[currentStair]=minCost;
+
+        return minCost;
+    }
+};
