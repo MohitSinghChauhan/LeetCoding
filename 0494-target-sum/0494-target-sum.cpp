e635de22323e71942a933ba6bcd0804e6e59382f
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-09-19 18:27:48 +0530

    Time: 314 ms (33.04%), Space: 90.6 MB (5.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 
 private:
-    int countTargetSumWays(int currIdx, vector<int>& nums, int target){
+    int countTargetSumWays(int currIdx, vector<int>& nums, int target, unordered_map<string, int>& memo){
 
         if(target == 0 && currIdx==nums.size())
             return 1;
@@ -9,15 +9,22 @@ private:
         if(currIdx == nums.size())
             return 0;
 
-        int includeWithPlus = countTargetSumWays(currIdx+1, nums, target-nums[currIdx]);
-        int includeWithMinus = countTargetSumWays(currIdx+1, nums, target+nums[currIdx]);
+        string currentKey = to_string(currIdx)+"-"+to_string(target);
 
-        return includeWithPlus + includeWithMinus;
+        if(memo.find(currentKey)!=memo.end()){
+            return memo[currentKey];
+        }
+
+        int includeWithPlus = countTargetSumWays(currIdx+1, nums, target-nums[currIdx], memo);
+        int includeWithMinus = countTargetSumWays(currIdx+1, nums, target+nums[currIdx], memo);
+
+        return memo[currentKey] = includeWithPlus + includeWithMinus;
     }
 
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int currIdx = 0;
-        return countTargetSumWays(currIdx, nums, target);
+        unordered_map<string,int> memo;
+        return countTargetSumWays(currIdx, nums, target, memo);
     }
 };
