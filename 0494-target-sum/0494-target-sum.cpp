11f6ad6a5f595cf8cf45b30cc449a27bdb2bcd94
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-09-22 19:01:52 +0530

    Time: 23 ms (55.32%), Space: 40.2 MB (21.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 
 private:
-   int count_target(vector<int> &nums , int curr_index , int curr_sum , int target)
+   int count_target(vector<int> &nums , int curr_index , int curr_sum , int target , vector<vector<int>> &dp)
    {
        if(curr_sum == target && curr_index == nums.size())
          return 1;
        if(curr_index >= nums.size())
          return 0;
+       
+       if(dp[curr_index][curr_sum+1000] != -1)
+           return dp[curr_index][curr_sum+1000];
 
-       int plus_sum = count_target(nums , curr_index+1 , curr_sum + nums[curr_index] , target);
-       int subt_sum = count_target(nums , curr_index+1 , curr_sum - nums[curr_index] , target);
+       int plus_sum = count_target(nums , curr_index+1 , curr_sum + nums[curr_index] , target , dp);
+       int subt_sum = count_target(nums , curr_index+1 , curr_sum - nums[curr_index] , target , dp);
 
-       return plus_sum + subt_sum;
+       return  dp[curr_index][curr_sum+1000] = plus_sum + subt_sum;
    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -20,8 +23,8 @@ public:
     
         // unordered_map<string,int> memo;
        
-        // vector<vector<int>> dp(n+1 , vector<int>(target+1 , -1));
+        vector<vector<int>> dp(n+1 , vector<int>(2005 , -1));
 
-        return count_target(nums, 0 , 0 ,  target);
+        return count_target(nums, 0 , 0 ,  target, dp);
     }
 };
