f253a55b3fc6a47afda8134ce1562d65bf1a97ec
Mohit Singh Chauhan <72142831+MohitSinghChauhan@users.noreply.github.com>
2023-10-02 05:37:24 +0530

    Time: 65 ms (56.43%), Space: 48 MB (79.15%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -4,12 +4,18 @@
  */
 var reverseWords = function(s) {
     let words = s.split(' ');
-    let ans = "";
 
-    for(let word of words){
-        ans+= word.split('').reverse().join('');
-        ans+=" ";
+    // for(let word of words){
+    //     word = word.split('').reverse().join('');
+    // }
+
+    // Because In JavaScript, when you use a for...of loop with an array, 
+    // the variable word becomes a copy of each element, and modifying it won't 
+    // affect the original array.
+
+     for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].split('').reverse().join('');
     }
 
-    return ans.slice(0,-1);
+    return words.join(' ');
 };
